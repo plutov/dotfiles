ceb143a751ad81eed70b3049376352162d845bb7
Alex Pliutau <a.pliutau@gmail.com>
2024-10-30 15:09:30 +0100

    yabai basics
    Signed-off-by: Alex Pliutau <a.pliutau@gmail.com>

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,15 +4,29 @@ function install {
 	rm -rf $HOME/.zshrc
 	rm -rf $HOME/.env
 	rm -rf $HOME/.config
+	rm -rf $HOME/.yabairc
 	cp -a ./.zshrc $HOME/
   	cp -a ./.env $HOME/
+    cp -a ./.yabairc $HOME/
     cp -aR ./.config $HOME/
 
     # install ohmyzsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
     # install nerdfont
     curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
     ~/.local/bin/getnf
+
+    # install golang
+    brew install golang
+
+    # install yabai
+    # brew install koekeishiya/formulae/yabai
+    # yabai --restart-service
+
+    # install simple-bar
+    mkdir -p ~/code/widgets
+    # git clone https://github.com/Jean-Tinland/simple-bar ~/code/widgets/simple-bar
 
     # Set git config author details
     git config --global user.email "a.pliutau@gmail.com"
@@ -24,6 +38,7 @@ function install {
 function save {
 	cp -a $HOME/.zshrc ./
 	cp -a $HOME/.env ./
+	cp -a $HOME/.yabairc ./
 	cp -aR $HOME/.config ./
 	echo "dotfiles saved."
 }
