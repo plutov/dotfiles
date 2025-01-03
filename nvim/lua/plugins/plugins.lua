fe3bafab3e2cfd5ae0f56a58d33d080518209438
plutov <a.pliutau@gmail.com>
2025-01-03 13:34:22 +0100

    chore: barbecue
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -183,4 +183,14 @@ return {
 			end, { desc = "Search Config Files" })
 		end,
 	},
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {},
+	},
 }
