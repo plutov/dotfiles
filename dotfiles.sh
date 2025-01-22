cd6d428bdba1dc56baa2a09e942af13307675817
plutov <a.pliutau@gmail.com>
2025-01-22 15:05:31 +0100

    zsh
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- dotfiles.sh
+++ dotfiles.sh
@@ -25,7 +25,6 @@ function install {
 
 	# install ohmyzsh plugins
 	git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
 
 	# install nerdfont
 	curl -fsSL https://raw.githubusercontent.com/getnf/getnf/main/install.sh | bash
