27d9f5f526a199c432f6538194f6d30fe0f3de99
plutov <a.pliutau@gmail.com>
2025-01-25 21:34:46 +0100

    path
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- dotfiles.sh
+++ dotfiles.sh
@@ -25,7 +25,7 @@ function install {
 	fi
 
 	# install ohmyzsh plugins
-	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 
 	# install nerdfont
