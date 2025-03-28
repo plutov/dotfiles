9d23acd12ddc637f2093f5451a08fef066e7b65e
plutov <a.pliutau@gmail.com>
2025-03-28 15:41:36 +0100

    diag
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,3 +31,12 @@ vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({sele
 vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
 	desc = "Search current word",
 })
+
+-- Show all diagnostics on current line in floating window
+vim.api.nvim_set_keymap("n", "<Leader>d", ":lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+-- Go to next diagnostic (if there are multiple on the same line, only shows
+-- one at a time in the floating window)
+vim.api.nvim_set_keymap("n", "<Leader>n", ":lua vim.diagnostic.goto_next()<CR>", { noremap = true, silent = true })
+-- Go to prev diagnostic (if there are multiple on the same line, only shows
+-- one at a time in the floating window)
+vim.api.nvim_set_keymap("n", "<Leader>p", ":lua vim.diagnostic.goto_prev()<CR>", { noremap = true, silent = true })
