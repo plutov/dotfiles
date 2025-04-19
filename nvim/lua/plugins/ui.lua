2228229831519558598f81a1a21a70cd75e04ede
plutov <a.pliutau@gmail.com>
2025-04-20 00:30:55 +0200

    ccc
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,3 +1,10 @@
+local colored_fts = {
+	"cfg",
+	"css",
+	"conf",
+	"lua",
+	"scss",
+}
 return {
 	{
 		"scottmckendry/cyberdream.nvim",
@@ -22,6 +29,25 @@ return {
 					lualine_z = { "location" },
 				},
 			})
+		end,
+	},
+	{
+		"uga-rosa/ccc.nvim",
+		ft = colored_fts,
+		cmd = "CccPick",
+		opts = function()
+			local ccc = require("ccc")
+
+			ccc.output.hex.setup({ uppercase = true })
+			ccc.output.hex_short.setup({ uppercase = true })
+
+			return {
+				highlighter = {
+					auto_enable = true,
+					filetypes = colored_fts,
+					lsp = false,
+				},
+			}
 		end,
 	},
 }
