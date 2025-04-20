36ad1ae341b40079317f32c28051b38e70938ef9
plutov <a.pliutau@gmail.com>
2025-04-20 12:58:15 +0200

    cyber colors
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -12,7 +12,7 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("lualine").setup({
-				options = { theme = "gruvbox_dark", section_separators = "", component_separators = "" },
+				options = { theme = "auto", section_separators = "", component_separators = "" },
 				sections = {
 					lualine_a = { "mode" },
 					lualine_b = {},
