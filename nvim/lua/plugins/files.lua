b95b35863f76f0e45cfe1816cbbc3c0fc100a585
plutov <a.pliutau@gmail.com>
2025-04-20 12:29:37 +0200

    lua files
    Signed-off-by: plutov <a.pliutau@gmail.com>

---
+++ nvim/lua/plugins/files.lua
@@ -0,0 +1,77 @@
+return {
+	{
+		"MagicDuck/grug-far.nvim",
+		opts = { headerMaxWidth = 80 },
+		cmd = "GrugFar",
+		keys = {
+			{
+				"<leader>sr",
+				function()
+					local grug = require("grug-far")
+					local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+					grug.open({
+						transient = true,
+						prefills = {
+							filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+						},
+					})
+				end,
+				mode = { "n", "v" },
+				desc = "Search and Replace",
+			},
+		},
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"MunifTanjim/nui.nvim",
+		},
+		cmd = "Neotree",
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>n",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), position = "right" })
+				end,
+				desc = "NeoTree",
+			},
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				defaults = { file_ignore_patterns = { "node_modules", ".git" } },
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons",
+		},
+		opts = {},
+	},
+}
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,10 +1,3 @@
-local colored_fts = {
-	"cfg",
-	"css",
-	"conf",
-	"lua",
-	"scss",
-}
 return {
 	{
 		"scottmckendry/cyberdream.nvim",
@@ -32,6 +25,105 @@ return {
 		end,
 	},
 	{
+		"nvimdev/dashboard-nvim",
+		lazy = false,
+		opts = function()
+			local logo = vim.fn.getcwd()
+
+			logo = string.rep("\n", 8) .. logo .. "\n\n"
+
+			local opts = {
+				theme = "doom",
+				hide = {
+					statusline = false,
+				},
+				config = {
+					header = vim.split(logo, "\n"),
+					center = {
+						{
+							action = "Lazy",
+							desc = " Lazy",
+							icon = "",
+							key = "l",
+						},
+						{
+							action = function()
+								vim.api.nvim_input("<cmd>qa<cr>")
+							end,
+							desc = " Quit",
+							icon = "",
+							key = "q",
+						},
+					},
+					footer = function()
+						local stats = require("lazy").stats()
+						local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+						return {
+							"⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms",
+						}
+					end,
+				},
+			}
+
+			for _, button in ipairs(opts.config.center) do
+				button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+				button.key_format = "  %s"
+			end
+
+			-- open dashboard after closing lazy
+			if vim.o.filetype == "lazy" then
+				vim.api.nvim_create_autocmd("WinClosed", {
+					pattern = tostring(vim.api.nvim_get_current_win()),
+					once = true,
+					callback = function()
+						vim.schedule(function()
+							vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+						end)
+					end,
+				})
+			end
+
+			return opts
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts_extend = { "spec" },
+		opts = {
+			spec = {
+				{
+					mode = { "n", "v" },
+					{ "<leader>f", group = "file/find" },
+					{ "<leader>s", group = "search" },
+					{
+						"<leader>b",
+						group = "buffer",
+						proxy = "<c-b>",
+						expand = function()
+							return require("which-key.extras").expand.buf()
+						end,
+					},
+					{
+						"<leader>w",
+						group = "windows",
+						proxy = "<c-w>",
+						expand = function()
+							return require("which-key.extras").expand.win()
+						end,
+					},
+				},
+				{
+					mode = { "n" },
+					{ "<leader>bp", "<cmd>bprevvious<cr>", desc = "Prev Buffer" },
+					{ "<leader>bn", "<cmd>bnext<cr>", desc = "Next Buffer" },
+					{ "<leader>bb", "<cmd>e #<cr>", desc = "Switch to Other Buffer" },
+					{ "<leader>bd", "<cmd>bd<cr>", desc = "Delete Buffer" },
+				},
+			},
+		},
+	},
+	{
 		"uga-rosa/ccc.nvim",
 		ft = colored_fts,
 		cmd = "CccPick",
@@ -40,6 +132,14 @@ return {
 
 			ccc.output.hex.setup({ uppercase = true })
 			ccc.output.hex_short.setup({ uppercase = true })
+
+			local colored_fts = {
+				"cfg",
+				"css",
+				"conf",
+				"lua",
+				"scss",
+			}
 
 			return {
 				highlighter = {
