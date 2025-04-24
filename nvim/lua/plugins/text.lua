ecfc8cc6cc56c263e3c8042a791c3f673cd9c997
plutov <a.pliutau@gmail.com>
2025-04-24 11:49:50 +0200

    grug
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/lua/plugins/text.lua
+++ nvim/lua/plugins/text.lua
@@ -12,12 +12,8 @@ return {
 				"<leader>sr",
 				function()
 					local grug = require("grug-far")
-					local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
 					grug.open({
 						transient = true,
-						prefills = {
-							filesFilter = ext and ext ~= "" and "*." .. ext or nil,
-						},
 					})
 				end,
 				mode = { "n", "v" },
