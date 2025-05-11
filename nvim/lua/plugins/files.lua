3ae157ff61eb03bac3421b4d1750f2355ef7d950
plutov <a.pliutau@gmail.com>
2025-05-11 09:38:37 +0200

    yazi
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -52,4 +52,33 @@ return {
 		},
 		opts = {},
 	},
+	{
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"folke/snacks.nvim",
+		},
+		keys = {
+			{
+				"<leader>-",
+				mode = { "n", "v" },
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+			{
+				"<leader>cw",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+		},
+		opts = {
+			open_for_directories = false,
+			keymaps = {
+				show_help = "<f1>",
+			},
+		},
+		init = function()
+			vim.g.loaded_netrwPlugin = 1
+		end,
+	},
 }
