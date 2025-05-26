0ed22c73133609a091f4e3eb031efc430a960191
plutov <a.pliutau@gmail.com>
2025-05-26 10:33:25 +0200

    no neotree
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -1,32 +1,5 @@
 return {
 	{
-		"nvim-neo-tree/neo-tree.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-		cmd = "Neotree",
-		opts = {
-			filesystem = {
-				filtered_items = {
-					visible = true,
-					hide_dotfiles = false,
-					hide_gitignored = false,
-				},
-			},
-		},
-		keys = {
-			{
-				"<leader>n",
-				function()
-					require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd(), position = "right" })
-				end,
-				desc = "NeoTree",
-			},
-		},
-	},
-	{
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
