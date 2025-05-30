4fd46779bc64ec5a61d7769f7aaf707ccc2374a7
plutov <a.pliutau@gmail.com>
2025-05-30 21:30:11 +0200

    fg
    Signed-off-by: plutov <a.pliutau@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,6 +10,7 @@ vim.opt.shiftwidth = 2
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find Git Files" })
 vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search Grep" })
 vim.keymap.set("n", "<leader>bf", builtin.buffers, { desc = "Open Buffers" })
 local fk_opts = {
