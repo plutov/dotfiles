85e9b21249a11d93ca06d0d41de10ea5c9df3797
plutov <a.pliutau@gmail.com>
2025-06-05 13:11:17 +0200

    dotfiles

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,13 +1,21 @@
 require("config.lazy")
 
 vim.wo.number = true
+
+-- No welcome screen
+vim.opt.shortmess:append("I")
+
+-- everything is unnamed
 vim.api.nvim_set_option("clipboard", "unnamed")
+
+-- Setup tabs and spaces
 vim.opt.listchars = { space = "·", tab = "  " }
 vim.opt.list = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
+-- Finder
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find Git Files" })
@@ -21,8 +29,10 @@ vim.keymap.set("n", "<leader>fc", function()
 	builtin.find_files(fk_opts)
 end, { desc = "Search Config Files" })
 
+-- Exit the terminal
 vim.api.nvim_set_keymap("t", "<ESC>", "<C-\\><C-n>", { noremap = true })
 
+-- Expand long diagnostics
 vim.api.nvim_set_keymap(
 	"n",
 	"<Leader>d",
