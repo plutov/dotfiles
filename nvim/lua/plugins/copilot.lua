adde6794a2215c02b395e57b6a919ca704fd9640
plutov <a.pliutau@gmail.com>
2025-12-15 10:22:13 +0100

    copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,28 +1,5 @@
--- Copilot completion.
 return {
 	{
-		"zbirenbaum/copilot.lua",
-		event = "InsertEnter",
-		cmd = "Copilot",
-		opts = {
-			-- The panel is useless.
-			panel = { enabled = false },
-			suggestion = {
-				auto_trigger = true,
-				hide_during_completion = false,
-				keymap = {
-					accept = "<C-.>",
-					accept_word = "<M-w>",
-					accept_line = "<M-l>",
-					next = "<M-]>",
-					prev = "<M-[>",
-					dismiss = "<C-/>",
-				},
-			},
-			filetypes = {
-				markdown = true,
-				yaml = true,
-			},
-		},
+		"github/copilot.vim",
 	},
 }
